ed2707c36837d4d3247de613389cbc72db1eaed4
AvileCoder <82064802+AvileCoder@users.noreply.github.com>
2021-04-08 03:25:00 +0200

    Add files via upload

--- SimpleAuthExample.py
+++ SimpleAuthExample.py
@@ -1,13 +1,19 @@
+#import every dependency that we need
 import os
 import SimpleAuthLib
 import time
+#import every dependency that we need
+
+#create menu
 def menu():
     print('Welcome thanks for using our software')
     input()
+#create menu
 
+#create login function using SimpleAuthLib
 def login():
     LicenceKeyForThisSoftware = input('Please give me your licence: ')
-    if(SimpleAuthLib.Auth('https://jefff-crack.firebaseio.com/.json', LicenceKeyForThisSoftware)):
+    if(SimpleAuthLib.Auth('https://my-python-project-b36f8-default-rtdb.firebaseio.com/.json', LicenceKeyForThisSoftware)):
         os.system('cls')
         menu()
     else:
@@ -16,5 +22,8 @@ def login():
         time.sleep(2)
         os.system('cls')
         login()
+#create login function using SimpleAuthLib
 
+#Executing login function on code start
 login()
+#Executing login function on code start
